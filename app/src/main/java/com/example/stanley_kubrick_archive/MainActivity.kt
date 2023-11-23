d381d408a96c5c7046c99fa4b0a9d5610a28f2ca
mejdi14 <mejdiclubiste@gmail.com>
2023-11-23 22:19:06 +0100

    add a new list

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.stanley_kubrick_archive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.stanley_kubrick_archive.data.Item
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,15 +26,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StanleyKubrickArchiveTheme {
-                // A surface container using the 'background' color from the theme
+                val items = listOf(
+                    Item(1, "Title 1", "Description 1"),
+                    Item(2, "Title 2", "Description 2"),
+                    // Add more items as needed
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ItemList(items = items)
                 }
             }
         }
+    }
+
+    @Composable
+    private fun SimpleList() {
+        LazyColumn(content = {
+
+        })
     }
 }
 
@@ -35,4 +55,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun ItemCard(item: Item) {
+    Card(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = item.title, style = MaterialTheme.typography.bodyMedium)
+            Text(text = item.description, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
+
+@Composable
+fun ItemList(items: List<Item>) {
+    LazyColumn {
+        items(items) { item ->
+            ItemCard(item)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/stanley_kubrick_archive/data/Item.kt
@@ -0,0 +1,3 @@
+package com.example.stanley_kubrick_archive.data
+
+data class Item(val id: Int, val title: String, val description: String)
