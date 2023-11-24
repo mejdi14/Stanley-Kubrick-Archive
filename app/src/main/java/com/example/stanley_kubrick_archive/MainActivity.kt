526eaacaff35f9460f867e499e402ffec7294665
mejdi14 <mejdiclubiste@gmail.com>
2023-11-24 14:53:14 +0100

    why that was hard"

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.stanley_kubrick_archive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
@@ -16,8 +22,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 import com.example.stanley_kubrick_archive.data.Item
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
 
@@ -29,11 +40,18 @@ class MainActivity : ComponentActivity() {
                 val items = listOf(
                     Item(1, "Title 1", "Description 1"),
                     Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
+                    Item(2, "Title 2", "Description 2"),
                     // Add more items as needed
                 )
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize().background(color = Color.Black),
+                    color = Color.Black
                 ) {
                     ItemList(items = items)
                 }
@@ -50,34 +68,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun ItemCard(item: Item) {
+fun ItemCard(item: Item, scale: Float) {
     Card(
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier
+            .height(130.dp)
+
     ) {
-        Column(
+        Surface(
+            color = item.color,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(text = item.title, style = MaterialTheme.typography.bodyMedium)
-            Text(text = item.description, style = MaterialTheme.typography.bodyMedium)
+                .fillMaxSize()
+        ){
+            Box {
+
+            }
         }
     }
 }
 
 @Composable
 fun ItemList(items: List<Item>) {
-    LazyColumn {
-        items(items) { item ->
-            ItemCard(item)
+    LazyColumn(
+
+    ) {
+        itemsIndexed(items) { index, item ->
+            val scale = 1f - (index * 0.02f).coerceAtMost(0.2f)
+            ItemCard(item, scale = scale)
         }
     }
 }
--- app/src/main/java/com/example/stanley_kubrick_archive/data/Item.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/data/Item.kt
@@ -1,3 +1,10 @@
 package com.example.stanley_kubrick_archive.data
 
-data class Item(val id: Int, val title: String, val description: String)
+import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
+
+data class Item(
+    val id: Int, val title: String, val description: String, val color: Color = Color(
+        Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)
+    )
+)
