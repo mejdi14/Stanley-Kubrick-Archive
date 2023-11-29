eadd98facb21e8ae187b7643163ca44c7643ed45
mejdi14 <mejdiclubiste@gmail.com>
2023-11-29 16:38:57 +0100

    toolbar with gradient color

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -1,37 +1,31 @@
 package com.example.stanley_kubrick_archive
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.times
 import com.example.stanley_kubrick_archive.data.Item
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,13 +43,37 @@ class MainActivity : ComponentActivity() {
                     Item(2, "Title 2", "Description 2"),
                 )
                 Surface(
-                    modifier = Modifier.fillMaxSize().background(color = Color.Black),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.Black),
                     color = Color.Black
                 ) {
-                    ItemList(items = items)
+                    HomeScreen(items)
                 }
             }
         }
+    }
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun HomeScreen(items: List<Item>) {
+        Scaffold(topBar = { GradientToolbar("hello") }) {
+            ItemList(items = items)
+        }
+    }
+
+    @Composable
+    fun GradientToolbar(title: String) {
+        val gradient = Brush.verticalGradient(
+            colors = listOf(Color.Blue, Color.Green),
+            startY = 0.0f,
+            endY = Float.POSITIVE_INFINITY
+        )
+        TopAppBar(
+            title = { Text(text = title) },
+            modifier = Modifier.background(gradient) // Apply the gradient here
+        )
     }
 
     @Composable
