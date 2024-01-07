96244544edab28948a54064fb74c90d37354f29a
mejdi14 <mejdiclubiste@gmail.com>
2024-01-07 23:38:18 +0100

    working on shared animation

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stanley_kubrick_archive
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
@@ -9,6 +10,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -24,11 +26,15 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -151,7 +157,8 @@ fun dpToPx(dp: Float, context: Context): Float {
 fun ItemCard(
     item: Item,
     index: Int,
-    listState: LazyListState
+    listState: LazyListState,
+    selectedCard: MutableState<Int?>
 ) {
     val itemHeightPx = dpToPx((270f - 124f), LocalContext.current)
 
@@ -175,7 +182,7 @@ fun ItemCard(
                 rotationX =
                     -dynamicRotation
                 cameraDistance = 33f
-            }
+            }.clickable { selectedCard.value = index },
 
 
     ) {
@@ -207,15 +214,47 @@ fun pxToDp(px: Float, context: Context): Float {
 @Composable
 fun SimpleItemList(items: List<Item>) {
     val listState = rememberLazyListState()
-
+    var selectedCard = remember { mutableStateOf<Int?>(null) }
+    var selectedItemBounds by remember { mutableStateOf<Rect?>(null) }
     LazyColumn(
         modifier = Modifier.padding(horizontal = 30.dp),
         state = listState,
         verticalArrangement = Arrangement.spacedBy((-124).dp),
     ) {
         itemsIndexed(items) { index, item ->
-            ItemCard(item, index, listState)
+            ItemCard(item, index, listState, selectedCard)
         }
     }
+    selectedCard.value?.let { index ->
+        CardDetailsScreen(index = index) { selectedCard.value = null }
+    }
 
+}
+
+@Composable
+fun CardDetailsScreen(index: Int, onClose: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .height(270.dp)
+            .fillMaxWidth()
+            .graphicsLayer {
+                rotationX = 0f
+            }.clickable { onClose() },
+
+
+        ) {
+        Surface(
+
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxSize(),
+                painter = painterResource(id = R.drawable.glory_paths),
+                contentDescription = "image",
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
 }
