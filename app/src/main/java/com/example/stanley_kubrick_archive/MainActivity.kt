5e057474db0c546ff460da3184897a586584ada7
mejdi14 <mejdiclubiste@gmail.com>
2024-03-01 11:26:34 +0100

    fix the rotation state

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -5,18 +5,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -24,21 +13,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -46,21 +29,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.boundsInRoot
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.stanley_kubrick_archive.component.CardDetailsScreen
 import com.example.stanley_kubrick_archive.component.CardItem
 import com.example.stanley_kubrick_archive.data.MovieCard
 import com.example.stanley_kubrick_archive.data.movieList
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
-import com.example.stanley_kubrick_archive.utils.movieCardDimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -153,6 +129,7 @@ fun dpToPx(dp: Float, context: Context): Float {
 fun SimpleItemList(items: List<MovieCard>) {
     val listState = rememberLazyListState()
     val selectedCard = remember { mutableStateOf<Int?>(null) }
+    val animationInProgress = remember { mutableStateOf<Boolean>(false) }
     val selectedItemBounds = remember { mutableStateOf<Rect?>(null) }
     LazyColumn(
         modifier = Modifier.padding(horizontal = 30.dp),
@@ -160,7 +137,7 @@ fun SimpleItemList(items: List<MovieCard>) {
         verticalArrangement = Arrangement.spacedBy((-124).dp),
     ) {
         itemsIndexed(items) { index, item ->
-            CardItem(item, index, listState, selectedCard, selectedItemBounds)
+            CardItem(item, index, listState, selectedCard, animationInProgress)
         }
     }
 }
--- app/src/main/java/com/example/stanley_kubrick_archive/component/CardItem.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/component/CardItem.kt
@@ -5,34 +5,27 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material3.Card
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.boundsInRoot
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.stanley_kubrick_archive.data.MovieCard
 import com.example.stanley_kubrick_archive.utils.movieCardDimension
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun CardItem(
@@ -40,7 +33,7 @@ fun CardItem(
     index: Int,
     listState: LazyListState,
     selectedCard: MutableState<Int?>,
-    selectedItemBounds: MutableState<Rect?>
+    animationInProgress: MutableState<Boolean>
 ) {
     val compositeState = remember {
         derivedStateOf {
@@ -63,40 +56,40 @@ fun CardItem(
         ), label = "AnimatedOffset"
     )
 
-    val dynamicRotation = if (index > compositeState.value.first) {
+
+    val initialRotationValue = if (index > compositeState.value.first) {
         20f * (index - compositeState.value.first) - (compositeState.value.second / itemHeightPx) * 20f
     } else {
         0f
     }
 
-    val rotationTarget = remember { mutableStateOf(-dynamicRotation) }
+    val targetRotationValue =
+        if (selectedCard.value != null && selectedCard.value == index) 0f else initialRotationValue
 
-    LaunchedEffect(key1 = selectedCard.value) {
-        rotationTarget.value = if (selectedCard.value == index) {
-            0f
-        } else {
-            -dynamicRotation
-        }
-    }
-
-    val animatedAdditionalRotation by animateFloatAsState(
-        targetValue = rotationTarget.value,
-        animationSpec = tween(
-            durationMillis = 800
-        ), label = "CardFlatRotation"
+    val dynamicRotation by animateFloatAsState(
+        targetValue = targetRotationValue,
+        animationSpec = tween(durationMillis = if (selectedCard.value != null || animationInProgress.value) 1000 else 0)
     )
+
+
     Card(
         modifier = Modifier
             .height((item.cardHeight).dp)
             .fillMaxWidth()
             .graphicsLayer {
-                rotationX = if((selectedCard.value == index))
-                    animatedAdditionalRotation else -dynamicRotation
+                rotationX =
+                    -dynamicRotation
                 cameraDistance = 33f
             }
             .clickable {
-                if (selectedCard.value == null) selectedCard.value = index else selectedCard.value =
-                    null
+                if (selectedCard.value == null) selectedCard.value = index else {
+                    GlobalScope.launch(Dispatchers.Main) { // Use GlobalScope for simplicity in this example
+                        selectedCard.value = null
+                        animationInProgress.value = true
+                        delay(1000)
+                        animationInProgress.value = false
+                    }
+                }
             }
             .offset(y = animatedOffset),
     ) {
