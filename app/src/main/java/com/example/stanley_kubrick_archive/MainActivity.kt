25e8157d5f4124952bc1dcdacc0a6a082ff68203
mejdi14 <mejdiclubiste@gmail.com>
2024-03-08 12:40:13 +0100

    what a pain in the ass

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.stanley_kubrick_archive
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -23,8 +26,10 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -34,6 +39,8 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
@@ -43,6 +50,7 @@ import com.example.stanley_kubrick_archive.component.ToolbarWithTextAndIcon
 import com.example.stanley_kubrick_archive.data.MovieCard
 import com.example.stanley_kubrick_archive.data.movieList
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
+import kotlinx.coroutines.launch
 import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +66,10 @@ class MainActivity : ComponentActivity() {
 
                 ) {
                     HomeScreen(items)
-                    ToolbarWithTextAndIcon(getString(R.string.toolbar_title), R.drawable.baseline_search_20)
+                    ToolbarWithTextAndIcon(
+                        getString(R.string.toolbar_title),
+                        R.drawable.baseline_search_20
+                    )
                     BottomGradientLayer(Modifier.align(Alignment.BottomStart))
                 }
             }
@@ -73,6 +84,7 @@ private fun HomeScreen(items: List<MovieCard>) {
 }
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MoviesCardsList(items: List<MovieCard>) {
     val listState = rememberLazyListState()
@@ -99,39 +111,108 @@ fun MoviesCardsList(items: List<MovieCard>) {
         }
         AnimatedVisibility(visible = selectedCard.value != null) {
             val stateList = rememberLazyListState()
-            var scrollOffset by remember { mutableStateOf(0) }
-
-            val list = mutableListOf<String>()
-            for(i in 1..100){
-              list.add("hello")
-        }
-            LazyColumn(modifier =  Modifier.pointerInput(Unit) {
-                detectVerticalSwipeGestures(onSwipeUp = {
-                    println("Swiped Up")
-                }, onSwipeDown = {
-                    println("Swiped Down")
-                })
+            val subListScrollEnabled = remember { mutableStateOf<Boolean>(false) }
+            val coroutineScope = rememberCoroutineScope()
+            var initialPadding by remember {
+                mutableStateOf(
+                    MovieCard(
+                        1,
+                        "",
+                        ""
+                    ).crossPathHeight + 90f
+                )
             }
-                .padding(top = 200.dp)){
-                items(list){
-                    Text("Hello this is the line $it", color = Color.White)
+            val list = mutableListOf<String>()
+            for (i in 1..100) {
+                list.add("hello")
+            }
+            Box(modifier = Modifier
+                .pointerInput(Unit) {
+                    detectVerticalSwipeGestures(
+                        onSwipeUp = {
+                            if ((initialPadding + it) >= 0)
+                                initialPadding += it
+                            else
+                                subListScrollEnabled.value = true
+                        },
+                        onSwipeDown = {
+                            if (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0)
+                                initialPadding += it
+                        },
+                    )
+                }
+                .padding(top = (initialPadding).dp)
+            ) {
+                LazyColumn(modifier = Modifier
+                    .pointerInput(Unit) {
+                        detectVerticalDragGestures { change, dragAmount ->
+                            if (((initialPadding + dragAmount) >= 0) && ((initialPadding + dragAmount) < 500)) {
+                                subListScrollEnabled.value = false
+                                initialPadding += dragAmount
+                            } else
+                                subListScrollEnabled.value = true
+                            coroutineScope.launch {
+                                // Your action here, e.g., refreshing content
+                                Log.d("TAG", "subListScrollEnabled: ")
+                            }
+                        }
+                        detectVerticalSwipeGestures(
+                            onSwipeDown = {
+                                Log.d("TAG", "onSwipeDown: ")
+                            },
+                            onSwipeUp = {
+                                Log.d("TAG", "onSwipeUp: ")
+                            }
+                        )
+
+                    }
+                    .pointerInteropFilter { motionEvent ->
+                        if (listState.firstVisibleItemIndex == 0) {
+                            // The logic inside here will depend on how you're detecting the swipe down gesture.
+                            // For simplicity, let's say any downward motion triggers the action:
+                            // You can refine this by tracking motion events more precisely.
+                            coroutineScope.launch {
+                                // Your action here, e.g., refreshing content
+                            }
+                        }
+                        false // Return false to indicate the event was not consumed, allowing for normal scrolling behavior
+                    },
+                    userScrollEnabled = subListScrollEnabled.value,
+                    state = stateList
+                ) {
+                    items(list) {
+                        Text("Hello this is the line $it", color = Color.White)
+                    }
                 }
             }
         }
     }
 }
 
+suspend fun Modifier.pointerInput(listState: LazyListState, detectSwipeDownGesture: suspend () -> Unit) = this.then(
+    Modifier.pointerInput(listState) {
+        awaitPointerEventScope {
+            while (true) {
+                val event = awaitPointerEvent()
+                val dragAmount = event.changes.first().positionChange().y
+                if (dragAmount > 0) { // Detecting swipe down
+                }
+            }
+        }
+    }
+)
+
 suspend fun PointerInputScope.detectVerticalSwipeGestures(
-    onSwipeUp: () -> Unit,
-    onSwipeDown: () -> Unit
+    onSwipeUp: (dragAmount: Float) -> Unit,
+    onSwipeDown: (dragAmount: Float) -> Unit,
 ) {
     detectDragGestures { change, dragAmount ->
         change.consumeAllChanges()
         if (abs(dragAmount.y) > abs(dragAmount.x)) { // Detect vertical movement
             if (dragAmount.y < 0) {
-                onSwipeUp()
+                onSwipeUp(dragAmount.y)
             } else {
-                onSwipeDown()
+                onSwipeDown(dragAmount.y)
             }
         }
     }
@@ -160,7 +241,10 @@ fun AdjustablePaddingLazyColumn(list: List<String>) {
 
                 // Use the captured density to convert Dp to Px and vice versa
                 val newPaddingPx = with(density) { topPadding.toPx() } - delta
-                topPadding = with(density) { newPaddingPx.coerceIn(with(density) { minTopPadding.toPx() }, with(density) { maxTopPadding.toPx() }).toDp() }
+                topPadding = with(density) {
+                    newPaddingPx.coerceIn(with(density) { minTopPadding.toPx() },
+                        with(density) { maxTopPadding.toPx() }).toDp()
+                }
 
                 // Adjust logic as needed to determine when to absorb scroll
                 return if (topPadding > minTopPadding) Offset.Zero else available
