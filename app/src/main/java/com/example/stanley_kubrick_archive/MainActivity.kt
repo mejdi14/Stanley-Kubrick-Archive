796970baaeb62f7a16e47698cb569ac8ba7c9529
mejdi14 <mejdiclubiste@gmail.com>
2024-03-14 14:41:02 +0100

    i was a bit afraid but yeah it will work

--- app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.stanley_kubrick_archive
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.updateTransition
 import androidx.activity.ComponentActivity
@@ -11,7 +10,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.expandIn
 import androidx.compose.animation.shrinkOut
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -19,64 +17,45 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.consumeAllChanges
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
+import com.example.stanley_kubrick_archive.component.ActorCardItem
 import com.example.stanley_kubrick_archive.component.BottomGradientLayer
 import com.example.stanley_kubrick_archive.component.MovieItem
 import com.example.stanley_kubrick_archive.component.ToolbarWithTextAndIcon
 import com.example.stanley_kubrick_archive.data.MovieCard
 import com.example.stanley_kubrick_archive.data.movieList
 import com.example.stanley_kubrick_archive.ui.theme.StanleyKubrickArchiveTheme
-import kotlinx.coroutines.launch
 import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
@@ -85,18 +64,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             StanleyKubrickArchiveTheme {
                 val items = movieList
+                val selectedCard = remember { mutableStateOf<Int?>(null) }
                 Box(
                     modifier = Modifier
                         .background(color = Color.Black)
                         .fillMaxSize()
 
                 ) {
-                    HomeScreen(items)
+                    HomeScreen(items, selectedCard)
                     ToolbarWithTextAndIcon(
                         getString(R.string.toolbar_title),
                         R.drawable.baseline_search_20
                     )
-                    BottomGradientLayer(Modifier.align(Alignment.BottomStart))
+                    if(selectedCard.value == null)
+                    BottomGradientLayer(Modifier.align(Alignment.BottomStart), selectedCard)
                 }
             }
         }
@@ -105,16 +86,16 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-private fun HomeScreen(items: List<MovieCard>) {
-    MoviesCardsList(items = items)
+private fun HomeScreen(items: List<MovieCard>, selectedCard: MutableState<Int?>) {
+    MoviesCardsList(items = items, selectedCard)
 }
 
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
-fun MoviesCardsList(items: List<MovieCard>) {
+fun MoviesCardsList(items: List<MovieCard>, selectedCard: MutableState<Int?>) {
     val listState = rememberLazyListState()
-    val selectedCard = remember { mutableStateOf<Int?>(null) }
+    val pagerState = rememberPagerState()
     val animationInProgress = remember { mutableStateOf<Boolean>(false) }
     val userScrollEnabled = remember { mutableStateOf<Boolean>(true) }
     Box {
@@ -131,13 +112,13 @@ fun MoviesCardsList(items: List<MovieCard>) {
                     listState,
                     selectedCard,
                     animationInProgress,
-                    userScrollEnabled
+                    userScrollEnabled,
+                    pagerState
                 )
             }
         }
         //DetailsMovie(selectedCard, listState)
-        //ExpandingCard()
-        val pagerState = rememberPagerState()
+
         AnimatedVisibility(visible = selectedCard.value != null) {
             Column(
                 modifier = Modifier
@@ -192,12 +173,12 @@ fun ExpandingCard() {
     var isExpanded by remember { mutableStateOf(false) }
     val transition = updateTransition(targetState = isExpanded, label = "cardTransition", )
     val cardSize = animateDpAsState(targetValue = if (isExpanded) 120.dp else 240.dp, label = "cardSize", animationSpec = tween<Dp>(
-        durationMillis = 3000, // 3 seconds for a slower animation
-        easing = LinearEasing // This is optional; adjust the easing function as needed
+        durationMillis = 3000,
+        easing = LinearEasing
     ) )
     val cardPadding = animateDpAsState(targetValue = if (isExpanded) 8.dp else 0.dp, label = "cardPadding", animationSpec = tween<Dp>(
-        durationMillis = 3000, // 3 seconds for a slower animation
-        easing = LinearEasing // This is optional; adjust the easing function as needed
+        durationMillis = 3000,
+        easing = LinearEasing
     ))
 
     Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
@@ -210,12 +191,12 @@ fun ExpandingCard() {
 
             if (isExpanded) {
                 Row {
-                    SmallCard(imageRes = R.drawable.clockwork_orange)
-                SmallCard(imageRes = R.drawable.bary)
+                    ActorCardItem(imageRes = R.drawable.clockwork_orange)
+                    ActorCardItem(imageRes = R.drawable.bary)
                 }
                 Row {
-                    SmallCard(imageRes = R.drawable.shining)
-                    SmallCard(imageRes = R.drawable.i)
+                    ActorCardItem(imageRes = R.drawable.shining)
+                    ActorCardItem(imageRes = R.drawable.i)
                 }
             } else {
                 Card(modifier = Modifier.size(240.dp)) {
--- app/src/main/java/com/example/stanley_kubrick_archive/component/CardItem.kt
+++ app/src/main/java/com/example/stanley_kubrick_archive/component/CardItem.kt
@@ -1,25 +1,43 @@
 package com.example.stanley_kubrick_archive.component
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.stanley_kubrick_archive.R
 import com.example.stanley_kubrick_archive.data.MovieCard
 import com.example.stanley_kubrick_archive.utils.cardsPositionSwitchAfterSelection
 import com.example.stanley_kubrick_archive.utils.transitionYAfterSelectionAnimation
@@ -37,7 +55,8 @@ fun MovieItem(
     listState: LazyListState,
     selectedCard: MutableState<Int?>,
     animationInProgress: MutableState<Boolean>,
-    userScrollEnabled: MutableState<Boolean>
+    userScrollEnabled: MutableState<Boolean>,
+    pagerState: PagerState
 ) {
     val compositeState = remember {
         derivedStateOf {
@@ -100,34 +119,76 @@ fun MovieItem(
         label = "CardRotationOnSelection"
     )
 
-    Card(
-        modifier = Modifier
-            .height((movieCard.cardHeight).dp)
-            .fillMaxWidth()
-            .graphicsLayer {
-                rotationX =
-                    -dynamicRotation
-                cameraDistance = movieCard.cardCameraDistance
-                translationY = dynamicTransitionY
-            }
-            .clickable {
-                GlobalScope.launch(Dispatchers.Main) {
-                    if (selectedCard.value == null) {
-                        selectedCard.value = index
-                        userScrollEnabled.value = false
-                    } else {
-                        selectedCard.value = null
-                        animationInProgress.value = true
-                        delay(movieCard.cardSelectionAnimationDuration)
-                        animationInProgress.value = false
-                        userScrollEnabled.value = true
-                    }
+    val cardPadding = animateDpAsState(targetValue = if (pagerState.currentPage > 0) 8.dp else 0.dp, label = "cardPadding", animationSpec = tween<Dp>(
+        durationMillis = 3000,
+        easing = LinearEasing
+    ))
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier
+        .height((movieCard.cardHeight).dp)
+        .fillMaxWidth()
+        .graphicsLayer {
+            rotationX =
+                -dynamicRotation
+            cameraDistance = movieCard.cardCameraDistance
+            translationY = dynamicTransitionY
+        }
+        .clickable {
+            GlobalScope.launch(Dispatchers.Main) {
+                if (selectedCard.value == null) {
+                    selectedCard.value = index
+                    userScrollEnabled.value = false
+                } else {
+                    selectedCard.value = null
+                    animationInProgress.value = true
+                    delay(movieCard.cardSelectionAnimationDuration)
+                    animationInProgress.value = false
+                    userScrollEnabled.value = true
                 }
             }
-            .offset(y = animatedOffset),
-    ) {
-        CardContent(movieCard, selectedCard)
+        }
+        .offset(y = animatedOffset),) {
+        Column(modifier = Modifier
+            .padding(all = cardPadding.value)
+
+            .animateContentSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            if (pagerState.currentPage > 0) {
+                Row {
+                    ActorCardItem(imageRes = R.drawable.clockwork_orange)
+                    ActorCardItem(imageRes = R.drawable.bary)
+                }
+                Row {
+                    ActorCardItem(imageRes = R.drawable.shining)
+                    ActorCardItem(imageRes = R.drawable.i)
+                }
+            } else {
+                Card(
+                    modifier = Modifier
+
+                ) {
+                    CardContent(movieCard, selectedCard)
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun ExpandingCard() {
+    var isExpanded by remember { mutableStateOf(false) }
+    val transition = updateTransition(targetState = isExpanded, label = "cardTransition", )
+    val cardSize = animateDpAsState(targetValue = if (isExpanded) 120.dp else 240.dp, label = "cardSize", animationSpec = tween<Dp>(
+        durationMillis = 3000,
+        easing = LinearEasing
+    ) )
+    val cardPadding = animateDpAsState(targetValue = if (isExpanded) 8.dp else 0.dp, label = "cardPadding", animationSpec = tween<Dp>(
+        durationMillis = 3000,
+        easing = LinearEasing
+    ))
+
+
 }
 
 
